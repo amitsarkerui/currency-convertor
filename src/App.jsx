76d77fade484f3ea5d4aa4150b13d7684ff53ade
Amit Sarker <you@example.com>
2024-06-21 00:38:52 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,93 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import useCurrencyInfo from "./hooks/useCurrencyInfo";
 import InputBox from "./components/InputBox";
 
 function App() {
-  const currency = useCurrencyInfo("usd");
+  const [amount, setAmount] = useState(1);
+  const [convertedAmount, setConvertedAmount] = useState();
+  const [from, setFrom] = useState("usd");
+  const [to, setTo] = useState("bdt");
+  const [loading, setLoading] = useState(true);
+  const [options, setOptions] = useState([]);
+
+  const currencyInfo = useCurrencyInfo(from);
+  console.log(amount);
+
+  useEffect(() => {
+    if (currencyInfo) {
+      setOptions(Object.keys(currencyInfo));
+      setLoading(false);
+      convert();
+    }
+  }, [currencyInfo, amount, to, from]);
+
+  const swap = () => {
+    setAmount(convertedAmount);
+    setConvertedAmount(amount);
+    setFrom(to);
+    setTo(from);
+  };
+
+  const convert = () => {
+    if (currencyInfo && currencyInfo[to]) {
+      setConvertedAmount(amount * currencyInfo[to]);
+    }
+  };
 
   return (
     <div className="bg-[url('./assets/bg/currency-bg.jpg')] w-full h-screen bg-cover bg-bottom">
       <div className="w-full h-screen backdrop-blur-sm bg-gray-950 bg-opacity-60 flex justify-center items-center">
         <div className="bg-[#180D3A] bg-opacity-90 w-full p-6 max-w-xl text-white rounded-xl border-2 border-gray-700">
-          <InputBox />
-          <div className="mx-auto text-center mt-7 mb-2 cursor-pointer">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={2}
-              stroke="currentColor"
-              className="size-10 inline bg-green-500 p-2 rounded-full"
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              convert();
+            }}
+          >
+            <InputBox
+              label="From"
+              currencyOptions={options}
+              loading={loading}
+              amount={amount}
+              selectCurrency={from}
+              onAmountChange={(amount) => setAmount(amount)}
+              onCurrencyChange={(currency) => setFrom(currency)}
+            />
+            <div
+              onClick={swap}
+              className="mx-auto text-center mt-7 mb-2 cursor-pointer"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5"
-              />
-            </svg>
-          </div>
-          <InputBox />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={2}
+                stroke="currentColor"
+                className="size-10 inline bg-[#271C2A] p-2 rounded-full hover:bg-green-600"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5"
+                />
+              </svg>
+            </div>
+            <InputBox
+              amount={convertedAmount}
+              label="To"
+              currencyOptions={options}
+              loading={loading}
+              selectCurrency={to}
+              onCurrencyChange={(currency) => setTo(currency)}
+              amountDisabled={true}
+            />
+            {/* <button
+              type="submit"
+              className="w-full bg-green-700 py-3 rounded-lg mt-6"
+            >
+              Convert
+            </button> */}
+          </form>
         </div>
       </div>
     </div>
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,23 +1,45 @@
 import React from "react";
 
-const InputBox = () => {
+const InputBox = ({
+  label,
+  currencyOptions = [],
+  loading,
+  selectCurrency = "usd",
+  amount,
+  onAmountChange,
+  onCurrencyChange,
+  amountDisabled,
+}) => {
   return (
     <div>
-      <p className="mb-2">From</p>
+      <p className="mb-2">{label}</p>
       <div className="flex gap-5">
         <input
           className="outline-none w-full bg-transparent py-2 border-2 border-gray-600 rounded-md px-3"
           type="number"
           placeholder="0"
+          value={amount.toFixed(2)}
+          onChange={(e) =>
+            onAmountChange && onAmountChange(Number(e.target.value))
+          }
+          disabled={amountDisabled}
         />
         <select
-          className="rounded-md px-3 py-2 cursor-pointer outline-none border-2 border-gray-600 bg-transparent "
+          className="rounded-md px-3 py-2 cursor-pointer outline-none border-2 border-gray-600 bg-transparent"
           name=""
           id=""
+          value={selectCurrency}
+          onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
         >
-          <option className="text-gray-600" value="">
-            USD
-          </option>
+          {loading ? (
+            <option>Loading...</option>
+          ) : (
+            currencyOptions.map((currency) => (
+              <option className="text-gray-600" key={currency} value={currency}>
+                {currency}
+              </option>
+            ))
+          )}
         </select>
       </div>
     </div>
